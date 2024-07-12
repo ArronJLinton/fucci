cf5a21d960206c7eb1720ca488e28fd94b96135d
Arron Linton <arronlinton@Arrons-MBP.home>
2024-07-12 13:53:18 -0400

    react-native init

--- App.tsx
+++ App.tsx
@@ -29,6 +29,7 @@ type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
+
 function Section({children, title}: SectionProps): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
@@ -71,25 +72,13 @@ function App(): React.JSX.Element {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <Section title="">
+            <Text>Fucci</Text>
+          </Section>  
         </View>
       </ScrollView>
     </SafeAreaView>
