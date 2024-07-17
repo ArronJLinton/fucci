0ecfe5f80b67d4419edd96b3daa3f87866baf8b8
Arron Linton <arronlinton@Arrons-MBP.fios-router.home>
2024-07-16 23:49:27 -0400

    displaying matches

--- app/screens/Matches/index.tsx
+++ app/screens/Matches/index.tsx
@@ -1,41 +1,87 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { ScrollView, Text, View, Image, StyleSheet } from 'react-native';
 
+const styles = StyleSheet.create({
+    section: {
+        flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'
+    }
+})
 const Matches = () => {
-
-    React.useEffect(() => {
+    const [matches, setMatches] = useState([])
+    useEffect(() => {
         const headers = {
             'Content-Type': 'application/json',
             'x-rapidapi-key': 'eHEXcBfXlvmshTKBzDaZsMjlg3vsp1S9P4tjsnkCPsukbJlULL',
-          };
+        };
         fetch('https://api-football-v1.p.rapidapi.com/v3/fixtures?date=2024-07-16', {
             method: 'GET',
             headers: headers
-          })
+        })
             .then(response => {
-                console.log('Data received:', response)
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
                 }
                 return response.json(); // parses JSON response into native JavaScript objects
             })
             .then(data => {
-                console.log('Data received:', data);
-                // Handle the data here
+
+                console.log('Data received:', JSON.stringify(data.response));
+                setMatches(data.response)
             })
             .catch(error => {
-                console.error('There was a problem with the fetch operation:', error)               
-
-
+                console.error('There was a problem with the fetch operation:', error)
                 // Handle errors here
             });
-    })
+    }, [])
 
-
+    type Match = {
+        teams: any;
+        score: any;
+    }
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Matches!</Text>
-        </View>
+        <ScrollView contentInsetAdjustmentBehavior="automatic" style={{ flex: 1 }}>
+            <View style={{ flex: 1 }}>
+                {matches.map((e: Match, i) => {
+                    const home = e.teams.home;
+                    const away = e.teams.away;
+                    return (
+                        <View key={i} style={{
+                            marginTop: 10, padding: 15, flexDirection: 'row', backgroundColor: 'white', justifyContent: 'space-between',
+                        }}>
+                            <View style={styles.section}>
+                                <Text>{home.name}</Text>
+                                <Image
+                                    style={{
+                                        marginLeft: 10,
+                                        width: 20,
+                                        height: 20,
+                                    }}
+                                    source={{ uri: home.logo }}
+                                />
+                            </View>
+
+                            <View style={styles.section}>
+                                <Text>{e.score.fulltime.home} - {e.score.fulltime.home}</Text>
+                            </View>
+
+                            <View style={styles.section}>
+                                <Text>{away.name}</Text>
+                                <Image
+                                    style={{
+                                        marginLeft: 10,
+                                        width: 20,
+                                        height: 20,
+                                    }}
+                                    source={{ uri: away.logo }}
+                                />
+
+                            </View>
+                        </View>
+                    )
+                }
+                )}
+            </View>
+        </ScrollView >
     );
 }
 
