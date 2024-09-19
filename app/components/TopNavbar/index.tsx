ef6ddcd341dd992102fb093493923bc3e7476ccb
Arron Linton <arronlinton@Arrons-MBP.fios-router.home>
2024-09-19 16:55:53 -0400

    dynamically rendering dates

--- app/components/TopNavbar/index.tsx
+++ app/components/TopNavbar/index.tsx
@@ -12,9 +12,26 @@ const styles = StyleSheet.create({
   },
 });
 
+const formatDate = (date: Date) => {
+  return date.toLocaleDateString('en-GB', {
+    weekday: 'short',  // 'Mon'
+    day: 'numeric',    // '19'
+    month: 'short',    // 'Sept'
+  }).replace(',', '');
+};
+
 const TopNavBar: React.FC = () => {
   const [selectedIndex, setSelectedIndex] = useState(4);
   const { state, setMatchDate } = useContext<MatchContextType>(MatchContext);
+  const date = new Date();
+  const threeDaysAgo = new Date();
+  const twoDaysAgo = new Date();
+  const twoDaysFromNow = new Date();
+  const threeDaysFromNow = new Date();
+  threeDaysAgo.setDate(date.getDate() - 3);
+  twoDaysAgo.setDate(date.getDate() - 2);
+  twoDaysFromNow.setDate(date.getDate() + 2);
+  threeDaysFromNow.setDate(date.getDate() + 3);
 
   return (
     <ScrollView
@@ -25,59 +42,48 @@ const TopNavBar: React.FC = () => {
         style={{ backgroundColor: 'transparent', alignContent: 'center' }}
         selectedIndex={selectedIndex}
         onSelect={i => {
-          const date = new Date();
-          const formattedDate = date.toISOString().split('T')[0];
           setSelectedIndex(i);
           switch (i) {
             case 0:
-              date.setDate(date.getDate() - 4);
-              setMatchDate(date.toISOString().split('T')[0]);
-              break;
-            case 1:
               date.setDate(date.getDate() - 3);
               setMatchDate(date.toISOString().split('T')[0]);
               break;
-            case 2:
+            case 1:
               date.setDate(date.getDate() - 2);
               setMatchDate(date.toISOString().split('T')[0]);
               break;
-            case 3:
+            case 2:
               date.setDate(date.getDate() - 1);
               setMatchDate(date.toISOString().split('T')[0]);
               break;
+            case 3:
+              date.setDate(date.getDate());
+              setMatchDate(date.toISOString().split('T')[0]);
+              break;
             case 4:
+              date.setDate(date.getDate() + 1);
+
               setMatchDate(date.toISOString().split('T')[0]);
               break;
             case 5:
-              date.setDate(date.getDate() + 1);
-              setMatchDate(date.toISOString().split('T')[0]);
-              break;
-            case 6:
               date.setDate(date.getDate() + 2);
               setMatchDate(date.toISOString().split('T')[0]);
               break;
-            case 7:
+            case 6:
               date.setDate(date.getDate() + 3);
-              setMatchDate(date.toISOString().split('T')[0]);
-              break;
-            case 8:
-              date.setDate(date.getDate() + 4);
               setMatchDate(date.toISOString().split('T')[0]);
               break;
             default:
               break;
           }
         }}>
-        {/* TODO: Dynamically create tab title text */}
-        <Tab style={styles.tab} title="Mon 15 Jul" />
-        <Tab style={styles.tab} title="Mon 15 Jul" />
-        <Tab style={styles.tab} title="Mon 15 Jul" />
+        <Tab style={styles.tab} title={formatDate(threeDaysAgo)} />
+        <Tab style={styles.tab} title={formatDate(twoDaysAgo)} />
         <Tab style={styles.tab} title="Yesterday" />
         <Tab style={styles.tab} title="Today" />
         <Tab style={styles.tab} title="Tomorrow" />
-        <Tab style={styles.tab} title="Fri 19 Jul" />
-        <Tab style={styles.tab} title="Fri 19 Jul" />
-        <Tab style={styles.tab} title="Mon 15 Jul" />
+        <Tab style={styles.tab} title={formatDate(twoDaysFromNow)} />
+        <Tab style={styles.tab} title={formatDate(threeDaysFromNow)}/>
       </TabBar>
     </ScrollView>
   );
