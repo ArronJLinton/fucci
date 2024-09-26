fdb8026b356130f47ff1dc07a06f2fdc129d794e
Arron Linton <arronlinton@Arrons-MBP.fios-router.home>
2024-09-26 15:38:43 -0400

    added GestureHandlerRootView

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 /**
  * @format
  */
-
+import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
 import App from './app/App';
 import { name as appName } from './app.json';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-AppRegistry.registerComponent(appName, () => App);
+const MainApp = () => (
+  <GestureHandlerRootView style={{ flex: 1 }}>
+    <SafeAreaProvider>
+      <App />
+    </SafeAreaProvider>
+  </GestureHandlerRootView>
+);
+
+AppRegistry.registerComponent(appName, () => MainApp);
